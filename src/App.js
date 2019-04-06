bca748abcb3551b051b3ea6b174fa88e3ea0f476
solariss13 <katrzyna.wiatrowska13@gmail.com>
2019-04-06 13:44:14 +0200

    fixing issue with mobile view in mobile devices using iOS Safari

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Div100vh from 'react-div-100vh';
 import RegionDetailsLoader from './components/RegionDetailsLoader';
 import HomePage from './components/HomePage';
 import './App.css';
@@ -10,14 +11,16 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="container">
-				<BrowserRouter>
-					<div>
-						<Route path={'/'} exact component={HomePage} />
-						<Route path={'/region/:handle'} component={RegionDetailsLoader} />
-					</div>
-				</BrowserRouter>
-			</div>
+      <Div100vh>
+        <div className="container">
+          <BrowserRouter>
+            <div>
+              <Route path={'/'} exact component={HomePage} />
+              <Route path={'/region/:handle'} component={RegionDetailsLoader} />
+            </div>
+          </BrowserRouter>
+        </div>
+      </Div100vh>
 		)
 	}
 }
